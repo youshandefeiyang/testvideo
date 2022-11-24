4bb7f641cddd8868349a4726a04c136e95876287
肥羊 <youshandefeiyang@gmail.com>
2022-11-24 22:50:36 +0800

    Add files via upload

--- douyin/zb.php
+++ douyin/zb.php
@@ -1,27 +1,10 @@
 <?php
 
 $id = $_GET['id'] ?? null;
+$quality = $_GET['quality'] ?? null;
 if ($id == 'douyin') {
     $firsturl = 'https://live.douyin.com/aweme/v1/web/backpack/match/list/?aid=6383&device_platform=web';
-    function get_info($geturl)
-    {
-        $headers = array(
-            "referer:https://live.douyin.com/",
-            'upgrade-insecure-requests: 1',
-            'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $geturl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $res = curl_exec($ch);
-        curl_close($ch);
-        return $res;
-    }
-
-    $matchcontent = get_info($firsturl);
+    $matchcontent = file_get_contents($firsturl);
     $arr = json_decode($matchcontent, true);
     $liveid = 0;
     foreach ($arr["match_list"] as $value) {
@@ -34,16 +17,16 @@ if ($id == 'douyin') {
         exit();
     }
     $liveurl = 'https://live.douyin.com/fifaworldcup/' . $liveid;
-    function get_roomid($url, $liveid)
+    function get_roomid($url)
     {
         $headers = array(
-            "referer:https://live.douyin.com/$liveid",
+            'referer:https://live.douyin.com/',
             'upgrade-insecure-requests: 1',
             'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
         );
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -52,18 +35,24 @@ if ($id == 'douyin') {
         return $data;
     }
 
-    $data = urldecode(get_roomid($liveurl, $liveid));
+    // $data = urldecode(get_roomid($liveurl));
+    $data = urldecode(file_get_contents($liveurl));
     $reg = "/\"roomid\"\:\"[0-9]+\"/i";
     preg_match($reg, $data, $roomid);
     $nreg = "/[0-9]+/";
     preg_match($nreg, $roomid[0], $realid);
     $mediaurl = "https://live.douyin.com/webcast/room/info_by_scene/?aid=6383&live_id=1&device_platform=web&language=zh-CN&enter_from=web_search&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=zh-CN&browser_name=Chrome&room_id=$realid[0]&scene=pc_stream_4k";
-    $flvcontent = get_info($mediaurl);
+    $flvcontent = file_get_contents($mediaurl);
     $narr = json_decode($flvcontent, true);
     $playarr = $narr["data"]["stream_url"]["live_core_sdk_data"]["pull_data"]["Flv"];
-    foreach ($playarr as $uhdvalue) {
-        if (in_array("uhd", $uhdvalue)) {
-            $playurl = $uhdvalue["url"];
+    if (empty($quality)) {
+        $realquality = "uhd";
+    } else {
+        $realquality = $quality;
+    }
+    foreach ($playarr as $qualityvalue) {
+        if (in_array($realquality, $qualityvalue)) {
+            $playurl = $qualityvalue["url"];
         }
     };
     header("Location: $playurl");
